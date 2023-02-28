aad2018e5232e20cbf6e5394e7004a8d0823d08a
Alex Horning <67214695+makeitouthill@users.noreply.github.com>
2023-02-27 17:37:24 -0800

    Add Reaction Model

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const {Schema} = require('mongoose');
+
+const reactionSchema = new Schema (
+    {
+        reactionBody: {
+            type: String,
+            require: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            require: true,
+        },
+        createdAt: {
+            type: Date.now,
+            get: (timestamp) => moment(timestamp).format('YYYY-MM-DD [Z] HH:mm:ss')
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+module.exports = reactionSchema;
